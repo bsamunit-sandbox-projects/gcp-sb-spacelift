164cd278de0a6805c30aa2e3909e4c98a5e6274b
Binu Thomas <102838305+bthomas-jha@users.noreply.github.com>
2023-09-01 15:04:39 -0400

    Update main.tf

--- managed-stack/main.tf
+++ managed-stack/main.tf
@@ -9,9 +9,15 @@ provider "google" {
   project      = "bsamunit-sandbox-projects"
 }
 
+variable "ENV_CODE" {
+  description = "A variable to hold Env Code"
+  type        = string
+  // Optionally, you can provide a default if necessary
+}
+
 # A sample storage bucket
 resource "google_storage_bucket" "my_bucket" {
-  name     = "bsamunit-gcp-sandbox-003"
+  name     = "bsamunit-gcp-sandbox-003" + var.ENV_CODE
   location = "US"
 
   storage_class = "STANDARD"
